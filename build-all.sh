ee70a548bbee4375741174119e22f9d96f4a05a7
Christopher Brown <cb@skeptomai.com>
2018-07-28 10:03:56 -0700

    update build script

--- build-all.sh
+++ build-all.sh
@@ -2,5 +2,5 @@
 
 cd base-notebook &&  docker build -f Dockerfile -t jupyter/base-notebook --force-rm . && cd ..
 cd minimal-notebook &&  docker build -f Dockerfile -t jupyter/minimal-notebook --force-rm . && cd ..
-cd scipy-notebook &&  docker build -f Dockerfile -t jupyter/scipy-notebook --force-rm . && cd ..
-
+cd scipy-notebook &&  docker build -f Dockerfile -t jupyter/scipy-notebook --force-rm .
+docker build -f Dockerfile-python2.7 -t skeptomai/scipy-notebook --force-rm . && cd ..
